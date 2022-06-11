a57d38146c159a4b867417adf39940a541bfce55
rahman-richardson <rrrichardsonr@gmail.com>
2022-06-11 16:13:11 -0300

    finished token and swap section

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,9 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap" rel="stylesheet" />
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {(this.props as any).emotionStyleTags}
         </Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,13 +15,13 @@ const Home: NextPage = () => {
           <ul className="navbar">
               <li><Link href="#home">Home</Link></li>
               <li><Link href="#about">About</Link></li>
-              <li><Link href="#service">Service</Link></li>
+              <li><Link href="#token">Token</Link></li>
               <li><Link href="#Swap">Swap</Link></li>
               <li><Link href="#contact">Contato</Link></li>
           </ul>
       </header>
 
-      <section className="home">
+      <section className="home" id="home">
         <div className="home-text">
             <h1>GdMoney</h1>
             <h2>Your <br /> marketplace outside the blockchain</h2>
@@ -32,7 +32,7 @@ const Home: NextPage = () => {
               <img 
                 src="/assets/img/felicidade.png" 
                 alt="Initial image"
-                />
+              />
             </div>
             <div className="base">
               <img 
@@ -70,13 +70,16 @@ const Home: NextPage = () => {
 
       <section className="about" id="about">
         <div className="about-img">
-            <img src="/assets/img/foguete.png" alt="" />
+            <img 
+              src="/assets/img/foguete.png" 
+              alt="" 
+            />
         </div>
 
         <div className="about-text">
-            <span>Sobre nós</span>
-            <h2>Trabalhamos com os <br /> melhores produtos</h2>
-            <p>Empresa que já está no ramo desde 2022, começando com marketplace e logo estaremos com nossa loja física.</p>
+            <span>About Us</span>
+            <h2>We work with the best products</h2>
+            <p>Fictitious company that has been in the business since 2022, starting with marketplace and soon we will be with our physical store.</p>
            
             <div className="block">
               <Button
@@ -85,19 +88,68 @@ const Home: NextPage = () => {
               >
                 Contact
               </Button>
-              <Button
+              <Link
                 href="https://www.investopedia.com/terms/b/blockchain.asp"
-                sx={{
-                  width: "13rem",
-                  height: "65px",
-                }}
-                variant="outlined"
+                target="_blank"
+                underline="none"
               >
-                What Is a Blockchain
-              </Button> 
+                <Button
+                  sx={{
+                    width: "13rem",
+                    height: "65px",
+                  }}
+                  variant="outlined"
+                >
+                  What Is a Blockchain
+                </Button> 
+              </Link>
             </div>                      
         </div>
       </section>  
+
+      <section className="token" id="token">
+        <div className="heading">
+          <img 
+              src="/assets/img/bnb.png" 
+              alt=""
+            />
+        </div>
+
+        <div className="container">
+            <div className="box">
+                <h2>BNB Token</h2>
+                <h3>
+                 Launched in July 2017, Binance is the biggest cryptocurrency exchange globally based on daily trading volume. Binance aims to bring cryptocurrency exchanges to the forefront of financial activity globally. The idea behind Binance’s name is to show this new paradigm in global finance — Binary Finance, or Binance.
+                 <br />
+                 Aside from being the largest cryptocurrency exchange globally, Binance has launched a whole ecosystem of functionalities for its users. The Binance network includes the Binance Chain, Binance Smart Chain, Binance Academy, Trust Wallet and Research projects, which all employ the powers of blockchain technology to bring new-age finance to the world. BNB is an integral part of the successful functioning of many of the Binance sub-projects.
+                </h3>
+            </div>
+        </div>
+      </section>
+
+      <section className="swap" id="swap">
+        <h2>Do swap if you don't have bnb token</h2>
+        <Link 
+          href="https://pancakeswap.finance/swap"
+          target="_blank"
+          underline="none"
+        >
+          <Button
+            sx={{
+              width: "13rem",
+              height: "65px",
+              border: "1px solid orange",
+              color: "#996607",
+              '&:hover': {
+                color: "#556cd6",
+              },
+            }}
+            variant="outlined"
+          >
+            SWAP
+          </Button> 
+        </Link>
+      </section>
 
       <footer>
         <h1>
