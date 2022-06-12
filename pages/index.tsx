849db83d4723a3b5a1c57a82cfbdca1da6112cec
rahman-richardson <rrrichardsonr@gmail.com>
2022-06-11 23:34:01 -0300

    add responsive code

--- pages/index.tsx
+++ pages/index.tsx
@@ -93,31 +93,34 @@ const Home: NextPage = () => {
               />
             </div>
         </div>
-        <Button
-          className="login-button"
-          sx={{
-            background: "#602f68",
-            border: "1px solid #4c0f5c",
-            color: "white",
-            height: "67px",
-            width: "10rem",
-            mt: "-7rem",
-          }}
-          variant="outlined"
-        >
-          Log in
-        </Button>
-        <Button
-          sx={{
-            width: "13rem",
-            height: "65px",
-            mt: "-7rem",
-            ml: "-14rem",
-          }}
-          variant="outlined"
-        >
-          Sign up
-        </Button>
+
+        <div className="button-groups">
+          <Button
+            className="login-button"
+            sx={{
+              background: "#602f68",
+              border: "1px solid #4c0f5c",
+              color: "white",
+              height: "67px",
+              width: "10rem",
+              mt: "-7rem",
+            }}
+            variant="outlined"
+          >
+            Log in
+          </Button>
+          <Button
+            sx={{
+              width: "13rem",
+              height: "65px",
+              mt: "-10rem",
+              ml: "10.5rem",
+            }}
+            variant="outlined"
+          >
+            Sign up
+          </Button>
+        </div>
       </section>
 
       <section className="about" id="about">
