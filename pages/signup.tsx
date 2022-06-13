5da84cb892bbd4460922c37f7c804b403407fbbc
rahman-richardson <rrrichardsonr@gmail.com>
2022-06-13 17:05:34 -0300

    fineshed login

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -28,8 +28,8 @@ type State = {
 }
   
 const validationSchema = yup.object().shape({
-    username: yup.string().required("The username is required").max(20, "O título precisa ter menosde 40 caracteres"),
-    password: yup.string().required("The password is required ").max(50, "A descrição precisa ter menosde 150 caracteres"),
+    username: yup.string().required("The username is required").max(20, "The username must be less than 20 characters"),
+    password: yup.string().required("The password is required ").max(50, "The password must be less than 50 characters"),
     repassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required()
 });
 
@@ -51,8 +51,7 @@ const Signup: NextPage = () => {
         },
         onSubmit: (values) => { signup() },
         validationSchema: validationSchema,
-    })
-
+    });
     /**-----------------------------------------------------------------------------**/
     const handleClickShowPassword = () => {
         setValues({
@@ -76,7 +75,6 @@ const Signup: NextPage = () => {
         event.preventDefault();
     };
     /**-----------------------------------------------------------------------------**/
-
     async function signup () {
         await api.post('/users/create', {
             username: formik.values.username,
@@ -96,129 +94,127 @@ const Signup: NextPage = () => {
             Router.push('/errors/signup');
         });      
     }
-
     /**-----------------------------------------------------------------------------**/
     return (
         <div className="main-signup">
-            <div className="box-content">
-              <div className="heading">
-                <header></header>
-                <h1>Sign Up</h1>
-              </div>
-                <div className="form"> 
-                 <form onSubmit={formik.handleSubmit}>
-                    <div className="field-username">
-                        <InputLabel
-                            htmlFor="username"
-                            sx={{ fontWeight: "bold" }}
-                        >
-                            username
-                        </InputLabel>
-                        <TextField 
-                           id="outlined-basic" 
-                           variant="outlined"
-                           sx={{
-                               width: "90%",
-                               height:"30px"
-                           }}
-                           onChange={(e) => formik.setFieldValue('username', e.target.value)}
-                           error={formik.touched.username && Boolean(formik.errors.username)}
-                        />
-                    </div>
-                    <div className="field-password">
-                        <InputLabel 
-                            htmlFor="outlined-adornment-password"
-                            sx={{ fontWeight: "bold" }}
-                        >
-                            Password
-                        </InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-password"
-                            type={values.showPassword ? 'text' : 'password'}
-                            value={formik.values.password}
-                            onChange={(e) => formik.setFieldValue('password', e.target.value)}
-                            error={formik.touched.password && Boolean(formik.errors.password)}
-                            endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
+         <div className="box-content">
+          <div className="heading">
+            <header></header>
+            <h1>Sign Up</h1>
+          </div>
+          <div className="form"> 
+           <form onSubmit={formik.handleSubmit}>
+            <div className="field-username">
+             <InputLabel
+                htmlFor="username"
+                sx={{ fontWeight: "bold" }}
+             >
+                username
+            </InputLabel>
+            <TextField 
+                id="outlined-basic" 
+                variant="outlined"
+                sx={{
+                    width: "90%",
+                    height:"30px"
+                }}
+                onChange={(e) => formik.setFieldValue('username', e.target.value)}
+                error={formik.touched.username && Boolean(formik.errors.username)}
+                helperText={formik.touched.username && formik.errors.username}
+            />
+            </div>
+            <div className="field-password">
+             <InputLabel 
+                htmlFor="outlined-adornment-password"
+                sx={{ fontWeight: "bold" }}
+             >
+                Password
+             </InputLabel>
+             <OutlinedInput
+                id="outlined-adornment-password"
+                type={values.showPassword ? 'text' : 'password'}
+                value={formik.values.password}
+                onChange={(e) => formik.setFieldValue('password', e.target.value)}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
                                 aria-label="toggle password visibility"
                                 onClick={handleClickShowPassword}
                                 onMouseDown={handleMouseDownPassword}
                                 edge="end"
-                                >
+                            >
                                 {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                                </IconButton>
-                            </InputAdornment>
-                            }
-                            sx={{
-                                width: "90%",
-                            }}
-                        />
-                    </div>
-                    <div className="field-repassword">
-                        <InputLabel 
-                            htmlFor="outlined-adornment-password"
-                            sx={{ fontWeight: "bold" }}
-                        >
-                            Password Confirm:
-                        </InputLabel>
-                        <OutlinedInput
-                            id="outlined-adornment-password-2"
-                            type={values2.showPassword ? 'text' : 'password'}
-                            value={formik.values.repassword}
-                            onChange={(e) => formik.setFieldValue('repassword', e.target.value)}
-                            error={formik.touched.repassword && Boolean(formik.errors.repassword)}
-                            endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={handleClickShowPassword2}
-                                    onMouseDown={handleMouseDownPassword2}
-                                    edge="end"
-                                >
+                            </IconButton>
+                        </InputAdornment>
+                }
+                sx={{ width: "90%" }}
+             />
+            </div>
+            <div className="field-repassword">
+             <InputLabel 
+                htmlFor="outlined-adornment-password"
+                sx={{ fontWeight: "bold" }}
+             >
+                Password Confirm:
+             </InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-password-2"
+                    type={values2.showPassword ? 'text' : 'password'}
+                    value={formik.values.repassword}
+                    onChange={(e) => formik.setFieldValue('repassword', e.target.value)}
+                    error={formik.touched.repassword && Boolean(formik.errors.repassword)}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword2}
+                                onMouseDown={handleMouseDownPassword2}
+                                edge="end"
+                            >
                                 {values2.showPassword ? <VisibilityOff /> : <Visibility />}
-                                </IconButton>
-                            </InputAdornment>
-                            }
-                            sx={{
-                                width: "90%",
-                            }}
-                        />
-                    </div>
-                    <div className="button"> 
-                        <Button
-                          type="submit"
-                          sx={{
-                            width: "15.2rem",
-                            height: "55px",
-                            background: "#602f68",
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    sx={{
+                        width: "90%",
+                    }}
+                />
+               </div>
+               <div className="button"> 
+                 <Button
+                    type="submit"
+                    variant="outlined"
+                    sx={{
+                        width: "15.2rem",
+                        height: "55px",
+                        background: "#602f68",
+                        color: "white",
+                        border: "1px solid #4c0f5c",
+                        transition: "all .5s ease-in-out",
+                        mt:"0.8rem",
+                        '&:hover': {
                             color: "white",
-                            border: "1px solid #4c0f5c",
-                            transition: "all .5s ease-in-out",
-                            mt:"0.8rem",
-                            '&:hover': {
-                                color: "white",
-                                background: "#874191"
-                            },
-                          }}
-                          variant="outlined"
-                        >
-                         SIGN UP 
-                        </Button>
-                    </div>
-                 </form>
-                </div>
-            </div>
-            <div className="group-images">
-                <img 
-                  className="package-img" 
-                  src="/assets/img/package.png" 
-                />
-                <img 
-                  className="discount-img" 
-                  src="/assets/img/discount.png" 
-                />
-            </div>
+                            background: "#874191"
+                        },
+                    }}
+                 >
+                    SIGN UP 
+                 </Button>
+               </div>
+           </form>
+          </div>
+          </div>
+          <div className="group-images">
+            <img 
+              className="package-img" 
+              src="/assets/img/package.png" 
+            />
+            <img 
+              className="discount-img" 
+              src="/assets/img/discount.png" 
+            />
+          </div>
         </div>
     )
 }
