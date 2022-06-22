a2fd43a301678d967cf1b1c23574ca5c9315528f
rahman-richardson <rrrichardsonr@gmail.com>
2022-06-22 12:40:42 -0300

    enable google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
 import theme from '../theme';
 import createEmotionCache from '../createEmotionCache';
-
+import "@fontsource/open-sans";
 export default class MyDocument extends Document {
   render() {
     return (
